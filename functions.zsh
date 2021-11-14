179322643e1bd1e21f9f38da7462b99fe30f538d
andriesfc <andriesfc@gmail.com>
2021-11-14 15:22:15 +0200

    Updated my scripts.

--- functions.zsh
+++ functions.zsh
@@ -1,4 +1,5 @@
 
+
 function date() {
   if [ "$1" = "-I" ]; then
     command date "+%Y-%m-%dT%H:%M:%S%z"
@@ -6,6 +7,7 @@ function date() {
    command date "$@"
   fi
 }
+
 
 
 function append_to_path() {
@@ -18,7 +20,14 @@ function current_dir_name()
 }
 
 function lst() {
-    exa -l -h --group-directories-first -U -F --accessed --time-style long-iso
+
+  local target="$1"
+
+  if [ -z $target ]; then
+    target="."
+  fi 
+
+   exa -l -h --group-directories-first -U -F --accessed --time-style long-iso "$target"
 }
 
 # Some interesting alias taken from https://www.thorsten-hans.com/5-types-of-zsh-aliases
@@ -26,3 +35,17 @@ function lst() {
 # open any file with the following extensions usig vscode
 alias -s {md,json,yaml,yml,xml,conf,ini}=code
 
+# A function to drop all files and folders
+# NB: Use with 
+function drop() {
+  what_to_drop="$@"
+  if [ -z $what_to_drop ]; then 
+    print "please specify which folders and/or files to drop using:"
+    print "drop <files-or-folders>"
+    return
+  fi  
+  for todo in $@; do 
+    print "now dropping: $todo"
+    (rm -fr "$todo")
+  done 
+}
